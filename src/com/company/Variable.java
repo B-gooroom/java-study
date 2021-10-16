7e3d3ae9f9098eb0d867079d63f65faadcd77c7e
B-gooroom <j.sean1316@gmail.com>
2021-10-16 11:09:54 +0900

    [#1] Variable - 변수를 사용하는 이유1

---
+++ src/com/company/Variable.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Variable {
+    public static String a = "1";
+    public static void main(String[] args) {
+        boolean v1 = true;
+        int v2 = 100;
+        String v3 = "abc";
+
+        // 변수 Read
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+
+        // 변수 Update
+        v1 = false;
+        v2 = -10;
+        v3 = "def";
+
+        // 변수 Read
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+
+        // 변수 Delete
+    }
+}
