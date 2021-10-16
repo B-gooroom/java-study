a1d65671175bd0b809a5411f32db455be837182f
B-gooroom <j.sean1316@gmail.com>
2021-10-16 11:41:05 +0900

    [#2] Constant - 상수 사용법

---
+++ src/com/company/Constant.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Constant {
+    public static void main(String[] args) {
+        final boolean c1 = true;
+        final int c2 = 100;
+        final String c3 = "abc";
+
+        // 상수 Read
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+    }
+}
